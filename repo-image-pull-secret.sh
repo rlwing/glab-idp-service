9623af7bfed45fb87633e73e1dbc91c6bc16b4f9
Rob Wing <rob.wing@galvanize.com>
2021-06-08 15:39:38 -0700

    Setup for pipeline

---
+++ repo-image-pull-secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry glab-identity-deploy-token \
+      --docker-username="gitlab+deploy-token-480242" \
+      --docker-password="Fj-SvsWFArxmLaKxcAgg" \
+      --docker-email="rob.wing@galvanize.com" \
+      --docker-server="registry.gitlab.com"
